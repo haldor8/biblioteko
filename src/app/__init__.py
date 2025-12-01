47fae128539b5529576d9c908efccbd8e9f7928f
Charles <charles-iacopino@hotmail.com>
2025-12-01 16:54:35 +0100

    [WIP] adding a secret key to the app

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+import os
 
 def create_app():
     app = Flask(__name__)
+
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
     # Import des Blueprints
     from app.routes.main_routes import main
